714097ea08eeff4035677469cb0a626734acb0c6
PalamarchukVladislav <47598655+PalamarchukVladislav@users.noreply.github.com>
2019-10-03 11:15:15 +0300

    Delete TodoBlockRepository

--- src/main/java/com/knighttodo/knighttodo/repository/TodoBlockRepository.java
+++
@@ -1,7 +0,0 @@
-package com.knighttodo.knighttodo.repository;
-
-import com.knighttodo.knighttodo.entity.TodoBlock;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TodoBlockRepository extends JpaRepository<TodoBlock, Long> {
-}
