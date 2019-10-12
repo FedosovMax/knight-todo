ab63ded222a3a9ec729bb4d8f67eced070da3def
Vladyslav <sfinxx111@gmail.com>
2019-10-12 11:44:27 +0300

    add 5 types of hardness to the Todo

--- src/test/java/com/knighttodo/knighttodo/factories/TodoFactory.java
+++ src/test/java/com/knighttodo/knighttodo/factories/TodoFactory.java
@@ -2,6 +2,7 @@ package com.knighttodo.knighttodo.factories;
 
 import com.knighttodo.knighttodo.entity.Todo;
 import com.knighttodo.knighttodo.entity.TodoBlock;
+import com.knighttodo.knighttodo.entity.enums.Hardness;
 import com.knighttodo.knighttodo.entity.enums.Scaryness;
 
 public class TodoFactory {
@@ -10,11 +11,13 @@ public class TodoFactory {
     public static final Long TODO_ID = 1L;
     public static final String TODO_NAME = "hard working";
     public static final Scaryness SCARYNESS_TODO = Scaryness.fromText("NOT_SCARY");
+    public static final Hardness HARDNESS_TODO = Hardness.fromText("NOT_HARD");
     public static final TodoBlock TODO_BLOCK = new TodoBlock(TODO_ID,TODO_NAME, TodoBlockFactory.TODO_LIST);
 
     public static final Long UPDATE_TODO_ID = 2L;
     public static final String UPDATE_TODO_NAME = "hard working 2";
     public static final Scaryness UPDATE_SCARYNESS_TODO = Scaryness.fromText("SCARY");
+    public static final Hardness UPDATE_HARDNESS_TODO = Hardness.fromText("HARD");
     public static final TodoBlock UPDATE_TODO_BLOCK = new TodoBlock(UPDATE_TODO_ID,UPDATE_TODO_NAME,TodoBlockFactory.UPDATE_TODO_LIST);
 
 
@@ -24,6 +27,7 @@ public class TodoFactory {
                 .id(TODO_ID)
                 .todoName(TODO_NAME)
                 .scaryness(SCARYNESS_TODO)
+                .hardness(HARDNESS_TODO)
                 .todoBlock(TODO_BLOCK)
                 .build();
     }
@@ -34,6 +38,7 @@ public class TodoFactory {
                 .id(UPDATE_TODO_ID)
                 .todoName(UPDATE_TODO_NAME)
                 .scaryness(UPDATE_SCARYNESS_TODO)
+                .hardness(UPDATE_HARDNESS_TODO)
                 .todoBlock(UPDATE_TODO_BLOCK)
                 .build();
     }
