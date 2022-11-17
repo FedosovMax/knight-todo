84fd00c7e4de572427705626280afb9d6277c53e
alex625144 <benechka1@gmail.com>
2022-11-17 23:40:02 +0200

    delete exception

--- todocore/src/main/java/com/knighttodo/todocore/exception/FindDayTodoOrderIdException.java
+++
@@ -1,9 +0,0 @@
-package com.knighttodo.todocore.exception;
-
-public class FindDayTodoOrderIdException extends RuntimeException{
-
-    public FindDayTodoOrderIdException(String message, Throwable cause) {
-        super(message,cause);
-    }
-}
-
