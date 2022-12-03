b2287e9feba8b9f90fe2b3787a26c96c5d62f49c
Oleksandr Opanasiuk <benechka1@gmail.com>
2022-12-03 11:46:15 +0200

    fix format

--- todocore/src/main/java/com/knighttodo/todocore/exception/UpdateOrderNumberException.java
+++ todocore/src/main/java/com/knighttodo/todocore/exception/UpdateOrderNumberException.java
@@ -1,7 +1,6 @@
 package com.knighttodo.todocore.exception;
 
 public class UpdateOrderNumberException extends RuntimeException {
-
     public UpdateOrderNumberException(String message, Throwable cause) {
         super(message, cause);
     }
